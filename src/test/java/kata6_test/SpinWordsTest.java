6b923f9bfa3c1ecab4e0388e64ad84f967f53d08
dimalahno <dimalahno@rambler.ru>
2022-07-11 21:31:24 +0600

    Add SpinWords solution.

---
+++ src/test/java/kata6_test/SpinWordsTest.java
@@ -0,0 +1,15 @@
+package kata6_test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import kata6.SpinWords;
+import org.junit.jupiter.api.Test;
+
+public class SpinWordsTest {
+
+    @Test
+    public void test() {
+        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+        assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+    }
+}
